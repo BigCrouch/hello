ba19c6ab28935339f2f11c6ee13bab0eff753761
Jared <jforsberg2@huskers.unl.edu>
2022-08-25 20:35:25 +0000

    partner

--- hello.c
+++ hello.c
@@ -13,5 +13,8 @@ int main(int argc, char **argv) {
 
   printf("Andrew Crouch!\n");
   printf("Computer engineering\n");
+  printf("JaredForsberg!\n");
+  printf("Computer Engineering\n");
+
   return 0;
 }
