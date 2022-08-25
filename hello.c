c2b8897e842ac4ff828a597f5588e1bdd227bd9b
Andrew Crouch <acrouch6@huskers.unl.edu>
2022-08-25 19:37:53 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Andrew Crouch
+ * acrouch6@huskers.unl.edu
+ * Date: 2022/08/25
+ *
+ * A file that says my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Andrew Crouch!\n");
+  printf("Computer engineering\n");
+  return 0;
+}
